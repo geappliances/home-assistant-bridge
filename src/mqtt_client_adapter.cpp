8877186e38e263cfb1cd3180bb01aa3e65abd518
Ryan Hartlage <ryanplusplus@gmail.com>
2024-11-22 13:08:46 -0500

    Better unknown error string

--- src/mqtt_client_adapter.cpp
+++ src/mqtt_client_adapter.cpp
@@ -117,7 +117,7 @@ static void update_erd_write_result(i_mqtt_client_t* _self, tiny_erd_t erd, bool
         break;
 
       default:
-        self->client->publish(topic.c_str(), "unknown", true);
+        self->client->publish(topic.c_str(), "unknown error", true);
         break;
     }
   }
